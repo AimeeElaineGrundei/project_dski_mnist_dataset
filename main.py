5a6d36e1f6bdab8f140af7656427cb4caada4eb6
AimeeElaineGrundei <elaine.grundei@gmail.com>
2025-11-09 19:57:30 +0100

    statistic added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import base64
 from io import BytesIO
 import io
+from typing import Counter
 from database_operations import insert_result, fetch_all, fetch_by_model, create_table
 import keras
 from PIL import Image, ImageOps
@@ -60,10 +61,48 @@ def history():
 
     return render_template("history.html", history=history_list)
 
-@app.route('/statistics')
+
+@app.route("/statistics")
 def statistics():
-    # results = fetch_all()
-    return render_template('statistics.html')
+    selected_model = request.args.get('model', 'all')
+    results = fetch_all() if selected_model == 'all' else fetch_by_model(selected_model)
+
+    total_predictions = len(results)
+    correct_predictions = sum([row[6] for row in results])
+
+    accuracy = round((correct_predictions / total_predictions * 100) if total_predictions else 0, 2)
+
+    predictions_counter = Counter()
+    accuracy_counter = {str(i): 0 for i in range(10)}
+    count_per_digit = {str(i): 0 for i in range(10)}
+
+    for row in results:
+        pred = str(row[4])  # predicted_label
+        true = str(row[5])  # true_label
+        correct = row[6]
+
+        predictions_counter[pred] += 1
+        count_per_digit[true] += 1
+        if correct:
+            accuracy_counter[true] += 100
+
+    # calculate accuracy per digit (if count=0 -> 0%)
+    accuracy_per_digit = {}
+    for digit in range(10):
+        digit_str = str(digit)
+        if count_per_digit[digit_str] > 0:
+            accuracy_per_digit[digit_str] = round(accuracy_counter[digit_str] / count_per_digit[digit_str], 2)
+        else:
+            accuracy_per_digit[digit_str] = 0
+
+    return render_template(
+        "statistics.html",
+        total_predictions=total_predictions,
+        correct_predictions=correct_predictions,
+        accuracy=accuracy,
+        predictions_per_digit=predictions_counter,
+        accuracy_per_digit=accuracy_per_digit
+    )
 
 @app.route('/predict', methods=['POST'])
 def predict():
